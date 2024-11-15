0c9b4fee57c9f2edc1c92826ceab9821ffdd3ab4
karthiks373 <lit2021012@iiitl.ac.in>
2024-11-16 03:32:57 +0700

    feat(splash-screen): basic splash screen component

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -3,12 +3,36 @@ import { DynamicSdk } from "@spheroid/dynamic"
 
 const HomePage = () => {
   return (
-    <main className="align-center relative flex min-h-screen flex-col justify-end">
-      <div className="z-10 mx-auto mb-20">
-        <DynamicSdk.DynamicWidget />
+    <main className="flex h-screen flex-col">
+      <div className="bg-primary-base flex grow items-center justify-center">
+        <></>
+      </div>
+      <div className="flex min-h-[40%] flex-col items-center justify-center bg-white p-8">
+        <h2 className="mb-4 text-2xl font-bold">Easy payments online</h2>
+        <p className="mb-6 text-center text-lg">Make your payment experience better today. No hidden fees</p>
+        <Widget />
       </div>
     </main>
   )
 }
 
 export default HomePage
+
+const Widget = () => {
+  return (
+    <>
+      <style>{`
+        #dynamic-widget {
+            width: 100%;
+            height: 100%;
+        }
+      `}</style>
+      <div className="relative flex w-full items-stretch">
+        <DynamicSdk.DynamicWidget
+          buttonClassName=""
+          buttonContainerClassName=""
+        />
+      </div>
+    </>
+  )
+}
--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -9,39 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        whites: {
-          wallet_modal_background: "#F8F9FB",
+        primary: {
+          base: "#F9EFE5",
         },
-      },
-      keyframes: {
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
